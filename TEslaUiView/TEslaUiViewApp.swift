d7e56cb32d885c063bc2205ba086e51d486dbdd9
SHOKHRUKH <79970642+shoh188@users.noreply.github.com>
2022-07-05 12:35:33 +0500

    Initial Commit

---
+++ TEslaUiView/TEslaUiViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TEslaUiViewApp.swift
+//  TEslaUiView
+//
+//  Created by Shokhrukh Egamov on 05.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct TEslaUiViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
